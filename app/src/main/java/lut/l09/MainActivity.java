83c8184899a89a9cffd1bc0d83cbc03e499afb03
LUT-MiskaRihu <Miska.Rihu@student.lut.fi>
2022-03-29 15:17:40 +0300

    UI Changes

--- app/src/main/java/lut/l09/MainActivity.java
+++ app/src/main/java/lut/l09/MainActivity.java
@@ -1,14 +1,33 @@
 package lut.l09;
 
+import androidx.annotation.RequiresPermission;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    Context context = null;
+    ArrayList<Theatre> arrTheatres = new ArrayList<Theatre>();
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = getApplicationContext();
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+    }
+
+    public void readXML(View view) {
+        ReadAndWriteXML.getTheaters();
     }
 }
