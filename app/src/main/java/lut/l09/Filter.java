cadff97259b59a82843c6581c57a1ad6afe795e2
Miska Rihu <miska.rihu@student.lut.fi>
2022-04-12 16:16:46 +0300

    Added new attributes

--- app/src/main/java/lut/l09/Filter.java
+++ app/src/main/java/lut/l09/Filter.java
@@ -1,4 +1,14 @@
 package lut.l09;
 
+import java.util.Calendar;
+
 public class Filter {
+    Calendar startDateTimeMin;
+    Calendar startDateTimeMax;
+    String title;
+    int location;
+
+    public Filter() {
+
+    }
 }
